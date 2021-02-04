e5d51dea241b66d3fab046a6af67c82313dec5a1
restanka1@gmail.com <restanka1@gmail.com>
2021-02-04 23:58:53 +0300

    Add project files.

---
+++ FaktoriyelÖdev/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaktoriyelÖdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, sonuc;
+            string g;
+
+            Console.WriteLine("Faktöriyeli alınacak pozitif bir tam sayıyı giriniz (q: çıkış):");
+            g = Console.ReadLine();
+
+            while (g != "q")
+            {
+                sonuc = 1;
+                a = Convert.ToInt32(g);
+                if (a == 0 || a > 0)
+                {
+                    for (int i = 1; i <= a; i++)
+                    {
+                        sonuc = i * sonuc;
+                    }
+
+                    Console.WriteLine("Sonuç: " + sonuc);
+
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen pozitif bir değer giriniz");
+                }
+
+                Console.WriteLine("Faktöriyeli alınacak pozitif bir tam sayıyı giriniz (q: çıkış):");
+                g = Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
